36fb8d7da1f0b12ac18c6862f513c52bc00424bf
Hass <hassmroueh@gmail.com>
2024-10-25 20:27:58 -0400

    cc-plantshop complete

--- src/components/NewPlantForm.js
+++ src/components/NewPlantForm.js
@@ -1,13 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 
-function NewPlantForm() {
+function NewPlantForm({onAddPlant}) {
+  const [nameInput , setNameInput]= useState("");
+  const [imgInput,setImgInput] = useState("");
+  const [priceInput, setPriceInput]= useState('');
+
+  function handleSubmit(e){
+    e.preventDefault();
+    const newPlant= {
+      name: nameInput,
+      image: imgInput,
+      price: parseFloat(priceInput)
+    }
+    fetch("http://localhost:6001/plants", {
+      method:"POST",
+      headers: {'Content-Type': 'application/json'},
+      body: JSON.stringify(newPlant)
+    })
+    .then(r=>r.json())
+    .then(plant => onAddPlant(plant))
+    setNameInput('');
+    setImgInput('');
+    setPriceInput('');
+  }
+
   return (
     <div className="new-plant-form">
       <h2>New Plant</h2>
-      <form>
-        <input type="text" name="name" placeholder="Plant name" />
-        <input type="text" name="image" placeholder="Image URL" />
-        <input type="number" name="price" step="0.01" placeholder="Price" />
+      <form onSubmit={handleSubmit}>
+        <input type="text" name="name" placeholder="Plant name" value={nameInput} onChange={e=>setNameInput(e.target.value)} />
+        <input type="text" name="image" placeholder="Image URL" value={imgInput}  onChange={e=>setImgInput(e.target.value)}  />
+        <input type="number" name="price" step="0.01" placeholder="Price" value={priceInput}  onChange={e=>setPriceInput(e.target.value)} />
         <button type="submit">Add Plant</button>
       </form>
     </div>
--- src/components/PlantPage.js
+++ src/components/PlantPage.js
@@ -1,16 +1,50 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import NewPlantForm from "./NewPlantForm";
 import PlantList from "./PlantList";
 import Search from "./Search";
 
 function PlantPage() {
+  const[plants, setPlants]= useState([]);
+  const[filteredPlants, setFilteredPlants]= useState([]);
+
+  useEffect(()=>{
+    fetch("http://localhost:6001/plants")
+    .then(r=>r.json())
+    .then(data=>{
+      setPlants(data)
+      setFilteredPlants(data)
+    })
+  }, [])
+
+function addPlant(newPlant){
+  const addedPlants = [...plants, newPlant];
+  setPlants(addedPlants);
+  setFilteredPlants(addedPlants);
+}
+
+function onSearch (search){
+  if (search ===""){
+    setPlants(filteredPlants)
+  }else{
+  const searchedPlants = plants.filter((plant)=> plant.name.toLowerCase().includes(search.toLowerCase()))
+  setPlants(searchedPlants)
+  }
+}
+
+
+
   return (
     <main>
-      <NewPlantForm />
-      <Search />
-      <PlantList />
+      <NewPlantForm onAddPlant={addPlant} />
+      <Search onSearch={onSearch} />
+      <PlantList plants={plants}  />
     </main>
   );
 }
 
 export default PlantPage;
+
+
+    /**As a user:
+    I can mark a plant as "sold out".
+    I can search for plants by their name and see a filtered list of plants. */
