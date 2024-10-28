f674230f57b1fefc6afccfed2007f3a8b1f5affa
Hass <hassmroueh@gmail.com>
2024-10-28 14:16:22 -0400

    Filter Functionality Revised

--- src/components/PlantPage.js
+++ src/components/PlantPage.js
@@ -5,37 +5,28 @@ import Search from "./Search";
 import UpdatePrice from "./UpdatePrice";
 function PlantPage() {
   const [plants, setPlants] = useState([]);
-  const [filteredPlants, setFilteredPlants] = useState([]);
+  const [searched, setSearched]= useState('');
   const [newPrice, setNewPrice] = useState('')
   const [plantId, setPlantId] = useState()
+
   useEffect(() => {
     fetch("http://localhost:6001/plants")
       .then(r => r.json())
-      .then(data => {
-        setPlants(data)
-        setFilteredPlants(data)
-      })
+      .then(data =>setPlants(data))
   }, [])
 
   function addPlant(newPlant) {
     const addedPlants = [...plants, newPlant];
     setPlants(addedPlants);
-    setFilteredPlants(addedPlants);
   }
 
   function onSearch(search) {
-    if (search === "") {
-      setPlants(filteredPlants)
-    } else {
-      const searchedPlants = plants.filter((plant) => plant.name.toLowerCase().includes(search.toLowerCase()))
-      setPlants(searchedPlants)
-    }
+    setSearched(search);
   }
 
   function onDelete(selectedPlant){
     const deletedPlant = plants.filter(plant=>plant.id !== selectedPlant.id)
     setPlants(deletedPlant)
-    setFilteredPlants(deletedPlant)
   }
   
   function onUpdate(newPrice){
@@ -48,15 +39,17 @@ function PlantPage() {
       }
     })
     setPlants(updatedPlants)
-    setFilteredPlants(updatedPlants)
   }
+
+  const displayedPlants = plants.filter((plant)=> plant.name.toLowerCase().includes(searched.toLowerCase()))
+
 
   return (
     <main>
       <NewPlantForm onAddPlant={addPlant} />
       <Search onSearch={onSearch} />
       <UpdatePrice price = {newPrice} setNewPrice ={setNewPrice} plantId = {plantId} onUpdate={onUpdate} />
-      <PlantList plants={plants} onDel={onDelete} setPrice={setNewPrice} setPlantId={setPlantId} />
+      <PlantList plants={displayedPlants} onDel={onDelete} setPrice={setNewPrice} setPlantId={setPlantId} />
     </main>
   );
 }
